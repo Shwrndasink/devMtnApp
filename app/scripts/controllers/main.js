c0c3074269d2688e4baa65393c5eea42aa318a81
Marc White <mwhite34@bruinmail.slcc.edu>
2014-08-19 18:54:05 -0600

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devMtnAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the devMtnAppApp
+ */
+angular.module('devMtnAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
